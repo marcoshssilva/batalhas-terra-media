30215139c14a73abc7ab5445d8d1f508f29161bb
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-04-23 18:14:36 -0300

    implementated ModoDoPersonagem

---
+++ src/main/java/br/com/marcoshssilva/batalhasterramedia/models/persons/enums/ModoDoPersonagemEnumType.java
@@ -0,0 +1,10 @@
+package br.com.marcoshssilva.batalhasterramedia.models.persons.enums;
+
+public enum ModoDoPersonagemEnumType {
+    ATAQUE_CURTO,
+    ATAQUE_LONGO,
+    MOVIMENTO,
+    PARADO,
+    DEFESA_CURTO,
+    DEFESA_LONGO
+}
