4735a8f77ba9a9b96061dc9c750b2a8244695d99
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-04-23 17:18:01 -0300

    implementated enum CasaDaNacaoEnumTypes

---
+++ src/main/java/br/com/marcoshssilva/batalhasterramedia/models/nation/enums/CasaDaNacaoEnumTypes.java
@@ -0,0 +1,23 @@
+package br.com.marcoshssilva.batalhasterramedia.models.nation.enums;
+
+public enum CasaDaNacaoEnumTypes {
+
+    CIRITH_UNGOL("Cirith Ungol"),
+    PORTAO_NEGRO("Portão Negro"),
+    ABISMO_DE_HELM("Abismo de Helm"),
+    MINAS_TIRITH("Minas Tirith"),
+    CONDADO("O Condado"),
+    FLORESTA_DAS_TREVAS("Floresta das Trevas"),
+    CARAS_GALADHON("Caras Galadhon"),
+    COLINAS_DE_FERRO("Colinas de Ferro");
+
+    private String nome;
+
+    CasaDaNacaoEnumTypes(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
