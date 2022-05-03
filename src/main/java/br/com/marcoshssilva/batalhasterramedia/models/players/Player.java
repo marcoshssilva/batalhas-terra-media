11d45cbf179a02dd1e6c2d0ce3f2496cbb6011bc
marcos.santana <76234459+marcoshssilva@users.noreply.github.com>
2022-05-03 20:38:35 -0300

    Update Player.java

--- src/main/java/br/com/marcoshssilva/batalhasterramedia/models/players/Player.java
+++ src/main/java/br/com/marcoshssilva/batalhasterramedia/models/players/Player.java
@@ -1,13 +1,22 @@
 package br.com.marcoshssilva.batalhasterramedia.models.players;
 
+import br.com.marcoshssilva.batalhasterramedia.models.controls.ControleDoJogador;
 import br.com.marcoshssilva.batalhasterramedia.models.persons.Personagem;
 
 public interface Player {
-
     /**
      * Refere-se ao personagem controlado pelo jogador
      * @return br.com.marcoshssilva.batalhasterramedia.models.persons.Personagem
      */
     public Personagem getPersonagem();
 
+    /**
+     * @return Implementação dos controles do jogador
+     */
+    public ControleDoJogador getControle();
+
+    /**
+     * @return Id do jogador da instancia
+     */
+    public String getPlayerId();
 }
