1d1cfec9dde9e829cdc3df7e1b48193e8d964712
marcos.santana <76234459+marcoshssilva@users.noreply.github.com>
2022-05-03 20:38:09 -0300

    Update CampoDeBatalha.java

--- src/main/java/br/com/marcoshssilva/batalhasterramedia/models/scenario/areas/CampoDeBatalha.java
+++ src/main/java/br/com/marcoshssilva/batalhasterramedia/models/scenario/areas/CampoDeBatalha.java
@@ -1,6 +1,7 @@
 package br.com.marcoshssilva.batalhasterramedia.models.scenario.areas;
 
 import br.com.marcoshssilva.batalhasterramedia.models.players.Player;
+import br.com.marcoshssilva.batalhasterramedia.models.scenario.zone.AreaDoCampo;
 
 public interface CampoDeBatalha {
     /**
@@ -16,6 +17,64 @@ public interface CampoDeBatalha {
      * dentro do campo de batalha.
      */
     public void adicionarPlayer(String playerId, Player player, Integer eixoX, Integer eixoY)
-            throws AreaDeCampoInexistenteException;
+            throws AreaDeCampoInexistenteException,
+                   AreaDeCampoHabitadaException;
 
+    /**
+     * move o personagem dentro do campo de batalha
+     *
+     * @param playerId | id do jogador que irá movimentar
+     * @param saltoX   | quantidade de casas que ira mover sobre o eixo X
+     * @param saltoY   | quantidade de casas que ira mover sobre o eixo Y
+     * @param sentido  | orientacao do movimento
+     *
+     * @throws AreaDeCampoNaoPodeMoverPersonagemException
+     *         Falha ocorrida quando nao é possivel mover o personagem
+     *         para a area desejada.
+     */
+    public void moverPlayer(String playerId, int saltoX, int saltoY, SentidoEnumType sentido)
+            throws AreaDeCampoNaoPodeMoverPersonagemException,
+                   AreaDeCampoHabitadaException,
+                   AreaDeCampoInexistenteException;
+
+    /**
+     * coordena um devido jogador a atacar uma determina area
+     * utilizando um ataque longo.
+     *
+     * @param playerId | id do jogador
+     * @param eixoX    | eixoX alvo
+     * @param eixoY    | eixoY alvo
+     *
+     * @throws AreaDeCampoNaoPodeAtacarZonaException
+     *         Ocorrido quando a zona esta distante e nao pode ser alvejada pelo ataque
+     *
+     * @throws AreaDeCampoNaoExisteJogadorRequisitadoException
+     *         Ocorrido quando o playerId nao existe dentro do campo de batalha
+     */
+    public void atacarAreaComAtaqueLongo(String playerId, int eixoX, int eixoY)
+            throws AreaDeCampoNaoPodeAtacarZonaException,
+            AreaDeCampoNaoExisteJogadorRequisitadoException, AreaDeCampoInexistenteException;
+
+    /**
+     * coordena um devido jogador a atacar uma determina area
+     * utilizando um ataque curto.
+     *
+     * @param playerId | id do jogador
+     * @param eixoX    | eixoX alvo
+     * @param eixoY    | eixoY alvo
+     *
+     * @throws AreaDeCampoNaoPodeAtacarZonaException
+     *         Ocorrido quando a zona esta distante e nao pode ser alvejada pelo ataque
+     *
+     * @throws AreaDeCampoNaoExisteJogadorRequisitadoException
+     *         Ocorrido quando o playerId nao existe dentro do campo de batalha
+     */
+    public void atacarAreaComAtaqueCurto(String playerId, int eixoX, int eixoY)
+            throws AreaDeCampoNaoPodeAtacarZonaException,
+                   AreaDeCampoNaoExisteJogadorRequisitadoException;
+
+    /**
+     * @return Array bidimensional de zonas do campo de batalha
+     */
+    public AreaDoCampo[][] getZonasDoCampoDeBatalha();
 }
